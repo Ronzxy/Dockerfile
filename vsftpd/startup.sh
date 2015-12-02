93135b37fa364e74e6a3949beb6b54fb16f1c8f2
zhangchaoren <zhangchaoren@openeasy.net>
2015-12-02 15:23:20 +0800

    增加vsftpd创建映像文件

---
+++ vsftpd/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Nginx auto docker in debian
+# Powered by cr: zhangchaoren@openeasy.net
+
+
+/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
---
+++ vsftpd/user_conf.d
@@ -0,0 +1,4 @@
+#虚拟用户根目录,根据实际情况修改
+local_root=/var/ftp
+virtual_use_local_privs=YES
+write_enable=YES
