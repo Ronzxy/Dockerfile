ad4cd4a27e256219074567a10f642e3e24f214d1
Mr.Zhang <zhangchaoren@outlook.com>
2017-03-19 21:14:12 +0800

    增加 nginx 易用性

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ ! -f "/usr/sbin/nginx" ]; then
+    cp /nginx/sbin/nginx /usr/sbin/
+fi
+
+if [ ! -f "/etc/nginx/nginx.conf" ]; then
+    cp -r /nginx/conf/* /etc/nginx/
+    cp -r /conf/* /etc/nginx/
+fi
+
+if [ ! -d "/usr/html" ]; then
+    cp -r /nginx/html /usr/html
+fi
+
 # 创建目录及修改权限
 mkdir -p /var/cache/nginx /var/log/nginx
 chown -R www:www /var/cache/nginx /var/log/nginx
